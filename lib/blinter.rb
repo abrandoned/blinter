f6e9804ca382997fc8e3e49528ea54ff93dae7c1
Brandon Dewitt <brandonsdewitt@gmail.com>
2011-11-09 09:48:25 -0700

    move blint def files to blints dir

--- lib/blinter.rb
+++ lib/blinter.rb
@@ -1,4 +1,4 @@
 require 'blinter/version'
 require 'blinter/ext/hash'
 require 'blinter/file_traverser'
-require 'blinter/rspec_blints'
+Dir.glob('blinter/blints/**/*.rb').each { |f| require f }
---
+++ lib/blinter/blints/ruby.rb
@@ -0,0 +1,6 @@
+module Blinter
+  class RubyBlint < Blinter::RipperRider
+    def case_when_then(line)
+    end
+  end
+end
